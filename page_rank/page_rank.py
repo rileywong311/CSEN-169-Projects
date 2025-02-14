d6326a434a0bebd2a7e0566bc6a119b9fe61830f
Riley Wong <rnwong@scu.edu>
2025-02-13 19:05:48 -0800

    Do final calculations

--- page_rank/page_rank.py
+++ page_rank/page_rank.py
@@ -1,11 +1,16 @@
 from collections import defaultdict
 import numpy as np
+import sys
 
-incoming_pages = defaultdict(set)
-outgoing_count = {}
+
+ifile = sys.argv[1]
+d = float(sys.argv[2])
+
+incoming_pages = defaultdict(set) # target page : links into it from other pages
+outgoing_count = {} # target page : count of outgoing links from the page
 seen = set()
 
-file = open("input2.txt")
+file = open(ifile)
 count = -1
 for line in file:
   node, edges = line.split(":")
@@ -17,17 +22,35 @@ for line in file:
   count = max(node, max(edges))
 file.close()
 
-print("Incoming Pages", dict(incoming_pages))
-print("Outgoing Counts", outgoing_count)
+# print("Incoming Pages -", dict(incoming_pages))
+# print("Outgoing Counts -", outgoing_count)
 
 
-M = np.zeros((count + 1, count + 1))
 
-for row in range(count + 1):
-  for col in range(count + 1):
+n = count + 1
+M = np.zeros((n, n))
+
+for row in range(n):
+  for col in range(n):
     if col in incoming_pages[row]:
       M[row][col] = 1/outgoing_count[col]
 
-print(M)
+# print(M)
 
 
+
+v_0 = v_prev = np.full((n, 1), 1/n)
+v_curr = d * np.matmul(M, v_prev) + (1 - d) * v_0
+# v_curr = np.matmul(M, v_prev)
+
+epsilon = 0.00001
+while(np.linalg.norm(v_curr - v_prev) > epsilon):
+  v_prev = v_curr
+  v_curr = d * np.matmul(M, v_prev) + (1 - d) * v_0
+  # v_curr = np.matmul(M, v_prev)
+
+# print(v_curr)
+# print("Total -", sum(v_curr))
+
+for val in v_curr:
+  print("{:.10f}".format(val[0]))
