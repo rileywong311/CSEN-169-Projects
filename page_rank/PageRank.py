342a0c129e6e83224ee92196ca82a1ad5c3ce513
Riley Wong <rnwong@scu.edu>
2025-02-17 14:39:28 -0800

    Add analytics

--- page_rank/PageRank.py
+++ page_rank/PageRank.py
@@ -26,7 +26,6 @@ file.close()
 # print("Outgoing Counts -", outgoing_count)
 
 
-
 n = count + 1
 M = np.zeros((n, n))
 
@@ -37,20 +36,40 @@ for row in range(n):
 
 # print(M)
 
-
-
 v_0 = v_prev = np.full((n, 1), 1/n)
 v_curr = d * np.matmul(M, v_prev) + (1 - d) * v_0
 # v_curr = np.matmul(M, v_prev)
 
+loops = 1
 epsilon = 0.00001
 while(np.linalg.norm(v_curr - v_prev) > epsilon):
   v_prev = v_curr
   v_curr = d * np.matmul(M, v_prev) + (1 - d) * v_0
+  loops += 1
   # v_curr = np.matmul(M, v_prev)
 
 # print(v_curr)
-# print("Total -", sum(v_curr))
 
 for val in v_curr:
-  print("{:.10f}".format(val[0]))
+  print("{:.10e}".format(val[0]))
+
+# exit()
+
+# print("HYPERPARAMETER")
+# print(f"d={d}")
+
+# print()
+
+# print("NUMBER OF LOOPS TO CONVERGE")
+# print(loops)
+
+# print()
+
+# print("TOTAL SUM\n", sum(v_curr))
+
+# print()
+
+# top_pages = sorted(range(len(v_curr)), key=lambda i: v_curr[i], reverse=True)
+# print("TOP PAGES")
+# for top_page in top_pages:
+#   print(f"P({top_page}) = {v_curr[top_page]}")
